6364f2d87ad90c4248a4d4a814e6456ecf693703
Coquerelle Arnaud <arnaudc@MacBook-Pro-de-Coquerelle.local>
2020-01-14 14:55:32 +0100

    first commit

---
+++ TriBagageApp/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace TriBagageApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
