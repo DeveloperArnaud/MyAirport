c8d53d954dc44cb98fc8841aee4fb775431fc547
marylinkana <kanabaudoin7@gmail.com>
2020-01-28 12:53:01 +0100

    add GraphQLAirport

---
+++ GaphQLAirport/AirportQuery.cs
@@ -0,0 +1,19 @@
+﻿using GraphQL.Types;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using TriBagageApp.Context;
+
+namespace GraphQLAirport
+{
+    public class AirportQuery : ObjectGraphType
+    {
+        public AirportQuery(MyAirportContext db)
+        {
+            Field<ListGraphType<BagageType>>(
+                "bagages",
+                resolve: context => db.Bagages.ToList());
+        }
+    }
+}
---
+++ GaphQLAirport/BagageType.cs
@@ -0,0 +1,20 @@
+﻿using GraphQL.Types;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using TriBagageApp;
+
+namespace GraphQLAirport
+{
+    public class BagageType : ObjectGraphType<Bagage>
+    {
+        public BagageType() {
+            Field(b => b.Id, type: typeof(IdGraphType)).Description("Clé primaire de Bagage");
+            Field(b => b.VolId, type: typeof(IdGraphType)).Description("Id du vol auquel le bagage est rattaché");
+            Field(b => b.CodeIATA, type: typeof(IdGraphType)).Description("Code du bagage");
+            Field(b => b.Poids, type: typeof(IdGraphType)).Description("Poids du bagage");
+
+        }
+    }
+}
